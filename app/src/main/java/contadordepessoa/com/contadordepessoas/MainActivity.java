83f4307b9bcaf44971ed6dfb4d14450c1003c5a1
Diogo <diogo.pires@am4.com.br>
2016-11-08 11:28:46 -0200

    add confirme for reset button

--- app/src/main/java/contadordepessoa/com/contadordepessoas/MainActivity.java
+++ app/src/main/java/contadordepessoa/com/contadordepessoas/MainActivity.java
@@ -1,6 +1,9 @@
 package contadordepessoa.com.contadordepessoas;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -57,13 +60,34 @@ public class MainActivity extends Activity {
 
         botaoReset.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
-                numeroPessoas=0;
-                numeroMulheres=numeroPessoas;
-                numeroHomens=numeroPessoas;
-                String msg=Integer.toString(numeroPessoas);
-                campoTexto.setText("Total: "+msg+" pessoas");
-                botaoMulher.setText(Integer.toString(numeroMulheres));
-                botaoHomem.setText(Integer.toString(numeroHomens));
+
+                        final AlertDialog alert = new AlertDialog.Builder(MainActivity.this).create();
+                        alert.setMessage("Voce deseja resetar o contador?");
+                        alert.setCancelable(true);
+                        alert.setButton(DialogInterface.BUTTON_POSITIVE,"OK",new DialogInterface.OnClickListener() {
+                             public void onClick(DialogInterface dialog, int id) {
+                                 numeroPessoas=0;
+                                 numeroMulheres=numeroPessoas;
+                                 numeroHomens=numeroPessoas;
+                                 String msg=Integer.toString(numeroPessoas);
+                                 campoTexto.setText("Total: "+msg+" pessoas");
+                                 botaoMulher.setText(Integer.toString(numeroMulheres));
+                                 botaoHomem.setText(Integer.toString(numeroHomens));
+                             }
+                        });
+
+                        alert.setButton(DialogInterface.BUTTON_NEGATIVE,"Cancelar",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                               alert.hide();
+                            }
+                        });
+
+                        alert.show();
+
+
+
+
+
             }
 
         });
